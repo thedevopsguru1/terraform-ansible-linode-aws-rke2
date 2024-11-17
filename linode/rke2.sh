dd6fdd7111ee7efffaf9f9a3a5f600730550bea9
Your Name <you@example.com>
2024-11-17 00:55:53 -0600

    sss

--- linode/rke2.sh
+++ linode/rke2.sh
@@ -2,7 +2,7 @@ terraform init
 terraform apply --auto-approve
 
 echo "Sleeping for 120 seconds!!....."
-#sleep 120
+sleep 360
 bash create_inventory.sh
 ANSIBLE_HOST_KEY_CHECKING=False ansible -b -i ansible/inventory all -m ping 
 
